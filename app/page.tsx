64c59089ea948b75c78bcdb16f6044aafd326b2a
chmahesh <chmahesh997@gmail.com>
2024-07-29 20:13:06 +0530

    portfolio

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className=" h-screen w-screen flex items-center justify-center font-normal text-4xl capitalize">
+      hello 👋🏻
+    </div>
+  );
+}
